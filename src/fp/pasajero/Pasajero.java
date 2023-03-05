4f75536be64711ce4d032ae8202ce493aef23860
Manuel Serrano <sernolo2003@gmail.com>
2023-03-05 21:26:22 +0100

    Mejora

--- src/fp/pasajero/Pasajero.java
+++ src/fp/pasajero/Pasajero.java
@@ -4,10 +4,9 @@ public class Pasajero {
 	//ATRIBUTOS
 	private Integer passengerId;
 	private Boolean survived;
-	private Integer pclass;
+	private Integer pclass, age;
 	private String name;
 	private Genero Sex;
-	private Age
 	private SibSp
 	private Parch
 	private Ticket
